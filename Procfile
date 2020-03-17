be76abbedbdcb08b0093e6ea3b65210a2ab8172d
Ubuntu <ubuntu@ip-172-31-32-162.ec2.internal>
2020-03-17 18:33:41 +0000

    Altered Procfile and added allowed host Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn ecommerce.wsgi:application
+web: gunicorn mhphotography-ms4.wsgi:application
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
+#import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0b1e701fdf9c47b1afd3ad420befad00.vfs.cloud9.us-east-1.amazonaws.com"]
+ALLOWED_HOSTS = ["0b1e701fdf9c47b1afd3ad420befad00.vfs.cloud9.us-east-1.amazonaws.com", "mhphotography-ms4.herokuapp.com/"]
 
 
 # Application definition
